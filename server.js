20c29e8d76bb41fa1ddcfeb67a9816a8115a62bd
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-09-16 20:28:49 +0530

    Added initial setup

---
+++ server.js
@@ -0,0 +1,5 @@
+const app = require('./src/app');
+
+const port = process.env.PORT;
+
+app.listen(port, () => console.log(`server listening on ${port}`));
---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const route = require('./routes/route');
+const cookieParser = require('cookie-parser');
+
+require('./db/connect');
+
+const app = express();
+app.use(cookieParser());
+app.use(express.json({ limit: '4mb' }));
+
+app.use('/api', route);
+
+module.exports = app;
